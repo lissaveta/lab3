2b9b28a0bca2d011657bf3fa419af85c1b8ba4a7
lissaveta <54947531+lissaveta@users.noreply.github.com>
2019-12-13 00:29:43 +0300

    Add files via upload

---
+++ lab3/src/lab3/n3.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class n3 {
+    public static void main(String[] args) {
+        float b=1;
+        float a=1;
+        float c;
+        System.out.println("Первые 10 цифр гармонического ряда =");
+        while (b < 10){
+            c=a/b;
+            b++;
+            System.out.println(c+"+"); }
+    }
+}
